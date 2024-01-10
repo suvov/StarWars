cd35a5b3cb113a97ace12fcb8fbe1f7682db0001
Vladimir Shutyuk <shookup@gmail.com>
2024-01-10 12:05:35 +0100

    Add project and launch files

---
+++ StarWars/Launch/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  StarWars
+//
+//  Created by Vladimir Shutyuk on 10/01/2024.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(
+    _: UIApplication,
+    didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    return true
+  }
+}
